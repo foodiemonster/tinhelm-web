560f87ebc8f62339cb19f2336f8c4fea51e1f907
foodiemonster <hyo.exe@gmail.com>
2025-07-09 10:30:45 +0100

    Removing deprecated modal js

--- scripts/ui.combatModal.js
+++
@@ -1,4 +0,0 @@
-// DEPRECATED: Combat modal UI is now replaced by board-based combat in ui.combatBoard.js
-export function showCombatModal() {
-    alert('Combat modal is deprecated. Use board-based combat.');
-}
